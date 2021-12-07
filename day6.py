c4b0609316773eb7954e3070a5c2604e7105fb2b
mantomas <tomasman1979@gmail.com>
2021-12-07 17:36:28 +0100

    some type annotations

--- day6.py
+++ day6.py
@@ -1,19 +1,20 @@
 import re
 from collections import defaultdict
+from typing import DefaultDict, List
 
 
-def parse_data(file_name):
+def parse_data(file_name: str) -> List[int]:
     with open(file_name, "r") as f:
         numbers = [int(i) for i in re.findall(r"\d+", f.read())]
         return numbers
 
 
 # PART 1 - slow version :D
-def part_one(initial: list, repeat: int) -> int:
+def part_one(initial: List[int], repeat: int) -> int:
     fishes = initial
     for _ in range(repeat):
-        new_lf_count = fishes.count(0)
-        new_fishes = []
+        new_lf_count: int = fishes.count(0)
+        new_fishes: List[int] = []
         for lf in fishes:
             if lf > 0:
                 new_fishes.append(lf - 1)
@@ -26,14 +27,14 @@ def part_one(initial: list, repeat: int) -> int:
 
 
 # PART 2 - mač betr
-def part_two(initial: list, repeat: int) -> int:
-    data = defaultdict(int)
+def part_two(initial: List[int], repeat: int) -> int:
+    data: DefaultDict[int, int] = defaultdict(int)
     # initial dataset
     for i in initial:
         data[i] += 1
     # counting days
     for _ in range(repeat):
-        new_fishes = data[0]
+        new_fishes: int = data[0]
         for i in range(8):
             data[i] = data[i + 1]
         data[6] += new_fishes
@@ -41,7 +42,7 @@ def part_two(initial: list, repeat: int) -> int:
     return sum(data.values())
 
 
-def main(file_name):
+def main(file_name: str):
     data = parse_data(file_name)
     l_fish = part_one(data, 80)
     l_fish_part_two = part_two(data, 256)
